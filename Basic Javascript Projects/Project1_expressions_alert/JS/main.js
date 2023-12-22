fecd00d580b8524dd3567db4af451a96e831d488
MWKDawes <152348123+MWKDawes@users.noreply.github.com>
2023-12-22 13:37:43 -0800

    Created index.html and JS file to link to it
    Created HTML file to link to a JS file which I populated with variables Sent1, Sent2, A, B, and C, invoked methods and expressions to display alert window and concatenate string together

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var Sent1 = ('This is the beginning of the string') //Define variable Sent1
+var Sent2 = (' and this is the end of the string')   //Define variable Sent2
+
+var A = ("\"Waddup\" World,"                        //Beginning of defining variables, starting with A
+        +" again?!"), B=(Sent1 + Sent2), C=(3+3)      //Defining Variables B and C
+
+window.alert(A);                                    //Invoking window.alert function to display variable in a popup window
+document.write(B);                                  //Invoking document.write function to transcribe variable onto HTML page
